7d0c5d2e76834fbee803510f1b2042b4ecd88cf1
Niels Cölle <niels@home>
2017-08-05 19:22:52 +0200

    Using parallel stream added

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,6 +13,7 @@ class Main {
 				.mapToObj(MyTask::new)
 				.collect(Collectors.toList());
 		runSequentially(tasks);
+		useParallelStream(tasks);
 	}
 
 	private static void runSequentially(List<MyTask> tasks) {
@@ -21,7 +22,19 @@ class Main {
 				.map(MyTask::calculate)
 				.collect(Collectors.toList());
 		long duration = (System.nanoTime() - start);
-		System.out.printf("Processed %d tasks in %d milliseconds\n", tasks.size(),  TimeUnit.NANOSECONDS.toMillis(duration));
+		System.out.println("Running sequentially");
+		System.out.printf("Processed %d tasks in %d milliseconds\n", tasks.size(), TimeUnit.NANOSECONDS.toMillis(duration));
+		System.out.println("result = " + result);
+	}
+
+	private static void useParallelStream(List<MyTask> tasks) {
+		long start = System.nanoTime();
+		final List<Integer> result = tasks.parallelStream()
+				.map(MyTask::calculate)
+				.collect(Collectors.toList());
+		long duration = (System.nanoTime() - start);
+		System.out.println("\nUsing parallelStream()");
+		System.out.printf("Processed %d tasks in %d milliseconds\n", tasks.size(), TimeUnit.NANOSECONDS.toMillis(duration));
 		System.out.println("result = " + result);
 	}
 }
